df7b763aa4736c9701b657b4a75d4090ee737d3d
Kippob <kimomatic661@gmail.com>
2026-09-21 18:51:12 -0400

    Attack, Hurt, and Death Animations for Emma
    making the offsets was HELL

--- scripts/data/actors/emma.lua
+++ scripts/data/actors/emma.lua
@@ -38,13 +38,39 @@ function actor:init()
 
     -- Table of sprite animations
     self.animations = {
-          ["battle/idle"]         = {"battle/idle", 1/8, true},
+         ["battle/idle"]         = {"battle/idle", 1/8, true},
+          
+
+         ["battle/attack"]       = {"battle/attack", 1/15, false},
+         ["battle/act"]          = {"battle/act", 1/15, false},
+         ["battle/spell"]        = {"battle/spell", 1/15, false, next="battle/idle"},
+         ["battle/item"]         = {"battle/item", 1/12, false, next="battle/idle"},
+         ["battle/spare"]        = {"battle/spell", 1/15, false, next="battle/idle"},
+
+         ["battle/attack_ready"] = {"battle/attackready", 0.2, true},
+         ["battle/act_ready"]    = {"battle/actready", 0.2, true},
+         ["battle/spell_ready"]  = {"battle/spellready", 0.2, true},
+         ["battle/item_ready"]   = {"battle/itemready", 0.2, true},
+         ["battle/defend_ready"] = {"battle/defend", 1/15, false},
+
+         ["battle/act_end"]      = {"battle/actend", 1/15, false, next="battle/idle"},
+
+         ["battle/hurt"]         = {"battle/hurt", 1/15, false, temp=true, duration=0.5},
+         ["battle/defeat"]       = {"battle/defeat", 1/15, false},
+         ["battle/swooned"]      = {"battle/defeat", 1/15, false},
+
+         ["battle/transition"]   = {"walk/right_1", 1/15, false},
+         ["battle/intro"]        = {"battle/intro", 1/15, false},
+         ["battle/victory"]      = {"battle/victory", 1/10, false},
+         ["battle/transition_out"] = {"battle/transition_out", 1/15, false},
     }
 
     -- Table of sprite offsets (indexed by sprite name)
     self.offsets = {
         ["walk/left"] = {-7, 0},
         ["walk/right"] = {-7, 0},
+        ["battle/attack"] = {-20, -23},
+        ["battle/attackready"] = {-20, -23},
     }
 end
 
